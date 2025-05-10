4c351498178caf6e87c57da0b232404800a78d84
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2025-05-10 13:26:35 +0300

    End of S.F. 101 > Chapter 5 - Odoo docs > Developer Tutorials > Server framework 101 > Chapter 5

---
+++ estate/models/estate_properties.py
@@ -0,0 +1,37 @@
+from odoo import models, fields
+
+class Properties(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Properties"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ]
+    )
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Cancelled'),
+        ],
+        default='new', required=True, copy=False
+    )
+    active = fields.Boolean(default=True)
